5e9fcf9771c1bb4edcca2a00f2d83153cfbcf189
Xtron135 <94394142+Xtron135@users.noreply.github.com>
2025-09-26 14:27:20 +0800

    Create input.py

---
+++ Python/input.py
@@ -0,0 +1,24 @@
+import RPi.GPIO as GPIO
+import time
+
+# Use BCM pin numbering
+GPIO.setmode(GPIO.BCM)
+
+# Set GPIO 18 as input with pull-down resistor
+BUTTON_PIN = 18
+GPIO.setup(BUTTON_PIN, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
+print("Waiting for button press...")
+
+try:
+    while True:
+        if GPIO.input(BUTTON_PIN) == GPIO.HIGH:
+            print("Button was pressed!")
+            time.sleep(0.2)  # Debounce delay
+        else:
+            time.sleep(0.05)  # Polling delay
+except KeyboardInterrupt:
+    print("Exiting program")
+
+finally:
+    GPIO.cleanup()
